a0eb2094799ef0e3f5a5ef8d7b7b6f3fde9ffa96
Tony Samperi <github@tonysamperi.it>
2021-04-30 23:46:47 +0200

    Revert "fix docker attempt 003"
    This reverts commit 80fe06d47a03dfaa5104e6ca64a29deab60e6986.

--- docker/workflow.sh
+++ docker/workflow.sh
@@ -2,4 +2,4 @@
 npm install
 npm run build
 npm run test
-bash <(curl -s https://codecov.io/bash) -Z -t 0eb75e7c-b708-4d35-8620-5e668d8ccf5d
+bash <(curl -s https://codecov.io/bash) -Z -t ${CODECOV_TOKEN}
