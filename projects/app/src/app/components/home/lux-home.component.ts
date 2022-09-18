ea81864c4ca82d95e36df9688a1822d04a206a56
Tony Samperi <github@tonysamperi.it>
2022-09-18 18:28:09 +0200

    DEVS * Updated to latest ts-luxon * Improved examples

--- projects/app/src/app/components/home/lux-home.component.ts
+++ projects/app/src/app/components/home/lux-home.component.ts
@@ -1,6 +1,6 @@
 import {Component} from "@angular/core";
 import {Title} from "@angular/platform-browser";
-import {DateTime} from "ts-luxon";
+import {DateTime, Duration} from "ts-luxon";
 
 interface LuxTheme {
     description: string;
@@ -20,7 +20,11 @@ export class LuxHomeComponent {
         {
             code: `import {{ DateTime } from "ts-luxon";
 
-DateTime.local().setZone('America/New_York').minus({{ weeks: 1 }).endOf('day').toISO();`,
+DateTime.local()
+    .setZone("America/New_York")
+    .minus({{ weeks: 1 })
+    .endOf("day")
+    .toISO();`,
             title: "Datetime ISO custom timezone",
             subtitle: "Timezone for New York, set 1 week ago, end of day",
             value: DateTime.local().setZone("America/New_York").minus({weeks: 1}).endOf("day").toISO()
@@ -36,18 +40,43 @@ DateTime.local().toFormat("DD/MM/YYYY @ HH:mm");`,
         {
             code: `import {{ DateTime } from "ts-luxon";
 
-DateTime.local().setZone('America/New_York').minus({{ weeks: 1 }).endOf('day').toISO();`,
-            title: "Datetime ISO custom timezone",
-            subtitle: "Timezone for New York, set 1 week ago, end of day",
-            value: DateTime.local().setZone("America/New_York").minus({weeks: 1}).endOf("day").toISO()
+DateTime.local(2017, 5, 15, 8, 30).toLocaleString();`,
+            title: "Specific DateTime from config",
+            subtitle: "Local date at specific date and time",
+            value: DateTime.local(2017, 5, 15, 8, 30).toLocaleString()
         },
         {
             code: `import {{ DateTime } from "ts-luxon";
 
-DateTime.local().toFormat("DD/MM/YYYY @ HH:mm");`,
-            title: "Datetime custom format",
-            subtitle: "DateTime with default timezone",
-            value: DateTime.local().toFormat("dd/MM/yyyy @ HH:mm")
+DateTime.local().diff(DateTime.local().minus({
+    day: 5,
+    hour: 3,
+    minute: 27
+})).toHuman({onlyHumanUnits: !0})
+`,
+            title: "Duration toHuman",
+            subtitle: "Print human readable durations",
+            value: DateTime.local().diff(DateTime.local().minus({
+                day: 5,
+                hour: 3,
+                minute: 27
+            })).toHuman({onlyHumanUnits: !0})
+        },
+        {
+            code: `import {{ DateTime } from "ts-luxon";
+
+DateTime.local().locale;`,
+            title: "Get system locale",
+            subtitle: "Check your locale",
+            value: DateTime.local().locale
+        },
+        {
+            code: `import {{ DateTime } from "ts-luxon";
+
+DateTime.local().plus({years: 1, days: 1}).toLocaleString();`,
+            title: "Add periods to a DateTime",
+            subtitle: "Can also subtract with `minus`",
+            value: DateTime.local().plus({years: 1, days: 1}).toLocaleString()
         }
     ];
     githubLink: string = "https://github.com/tonysamperi/ts-luxon";
@@ -62,11 +91,17 @@ DateTime.local().toFormat("DD/MM/YYYY @ HH:mm");`,
     constructor(titleService: Title) {
         this.selectedTheme = this.themes[0];
         titleService.setTitle(this.appName);
+        this._foo();
     }
 
     updateTheme(theme: LuxTheme): void {
         this.selectedTheme = theme;
         document.body.classList.toggle("dark-theme", !!theme.value);
+    }
+
+    // Helper fn to try code before writing a new code block
+    private _foo(): void {
+
     }
 
 }
