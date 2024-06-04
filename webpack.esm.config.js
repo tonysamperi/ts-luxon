59694edcaebcedd5348e5e6606c9f6a096ce3b5a
otaconkiko <otaconkiko@gmail.com>
2024-06-04 20:51:04 +0000

    Stable

---
+++ webpack.esm.config.js
@@ -0,0 +1,30 @@
+const { resolve } = require("path");
+const webpackCommon = require("./webpack.common");
+
+webpackCommon.module.rules.push({
+    test: /\.tsx?$/,
+    use: {
+        loader: 'ts-loader',
+        options: {
+            configFile: "tsconfig.esm.json"
+        }
+    },
+    exclude: /node_modules/
+});
+
+module.exports = {
+    ...webpackCommon,
+    entry: {
+        'ts-luxon.esm': './src/index.ts'
+    },
+    experiments: {
+        outputModule: true,
+    },
+    output: {
+        path: resolve('dist', "esm"),
+        filename: '[name].mjs',
+        library: {
+            type: "module"
+        }
+    }
+};  
